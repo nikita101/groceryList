2411ef49ccb1a3832f75fe277057e54c6f22f52b
Nikita Odnoralov <nikitaodnoralov@Nikitas-MBP.home>
2021-06-11 13:06:20 -0400

    Created state and submit logic and wired up form.

--- grocery-list/src/Components/CheckoutCart.jsx
+++ grocery-list/src/Components/CheckoutCart.jsx
@@ -1,18 +1,25 @@
-import react from 'react'
+import React, {useState} from 'react'
 
 const CheckoutCart = () =>{
+const [cartItems, setCartItems] = useState('');
 
-    const onSubmit= ()=>{
+    const handleChange = (e) =>{
+        setCartItems({value: e.target.value});
+      }
 
-    }
+    const handleSubmit = (e) =>{
+        console.log(cartItems)
+        e.preventDefault();
+      }
 
     return (
-        <form>
+        <form onSubmit={handleSubmit}>
             <p>Please enter all the items purchased seperated by a comma. </p>
-            <input className="checkoutInput" type="text"/>
+            <input className="checkoutInput" type="text" onChange={handleChange}/>
+
             <button type="submit" className="myButton"> Checkout </button>
         </form>
-    )
+    );
 }
 
 export default CheckoutCart
