15a8b850dcf39a0d9d8a4da6a4c5b2bcdc0857d9
Nikita Odnoralov <nikitaodnoralov@Nikitas-MBP.home>
2021-06-13 11:37:49 -0400

    Created data normalization functions and gross calculator.

--- grocery-list/src/Components/CheckoutCart.jsx
+++ grocery-list/src/Components/CheckoutCart.jsx
@@ -1,16 +1,68 @@
-import React, {useState} from 'react'
+import {useState, useEffect} from 'react'
+import groceryItems from '../Data/groceryItems.json'
 
 const CheckoutCart = () =>{
 const [cartItems, setCartItems] = useState('');
+const [cartItemsAndQuantity, setCartItemsAndQuantity] = useState('');
+const [compiledCart, setCompiledCart] = useState('');
+const duplicateCounts = {};
+
+    useEffect(() => {
+        return cartItemsAndQuantity != '' ? compileCart() : null
+    },[cartItemsAndQuantity]);
+
+    useEffect(() => {
+        return compiledCart != '' ? calculateGrossTotal() : null
+    },[compiledCart]);
 
     const handleChange = (e) =>{
         setCartItems({value: e.target.value});
       }
 
+    const compileCart = () =>{
+        let grossCartAccumulator = []
+        for(let i = 0; i < groceryItems.groceryPrices.length; i++){
+            for(let x = 0; x < cartItemsAndQuantity.length; x++){
+                if(groceryItems.groceryPrices[i].name === cartItemsAndQuantity[x][0]){
+                    grossCartAccumulator.push({'item': groceryItems.groceryPrices[i].name,
+                    'quantity': cartItemsAndQuantity[x][1],
+                    'price': groceryItems.groceryPrices[i].price})
+                }
+            }
+        }
+        setCompiledCart(grossCartAccumulator)
+      }
+
+    const calculateGrossTotal = () =>{
+        const grossTotal = compiledCart.reduce( ( sum, { price, quantity } ) => sum + (price * quantity) , 0)
+        console.log(grossTotal)
+    }
+
     const handleSubmit = (e) =>{
-        console.log(cartItems)
+        const normalizedCart = cartItems.value.split(",").map(el => el.toLowerCase().trim())
+        const countedItemsArray = normalizedCart.reduce((allItems, item) => {
+            item in allItems ? allItems[item]++ : allItems[item] = 1
+          return allItems
+        }, {})
+        setCartItemsAndQuantity(Object.entries(countedItemsArray))
+
+
+        normalizedCart.filter((item, index)=>normalizedCart.indexOf(item) != index).forEach(function (x) { duplicateCounts[x] = (duplicateCounts[x] || 0) + 1; })
+
+        if (duplicateCounts && Object.keys(duplicateCounts).length === 0 && duplicateCounts.constructor === Object){
+            // duplicateCounts.forEach(function (item) { console.log(item) })
+            }
         e.preventDefault();
       }
+
+      // display the cart
+    //   for (let [key, value] of Object.entries(cartItemsAndQuantity)) {
+    //     console.log(key + ':' + value);
+    //   }
+    // or
+    // compiledCart.reduce((accumulator, currentValue,) => {
+    //     console.log(currentValue.item, currentValue.price * currentValue.quantity)
+    // }, {})
 
     return (
         <form onSubmit={handleSubmit}>
